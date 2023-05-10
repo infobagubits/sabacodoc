e71be5154e968cabf6a683d7ec34086864ca24df
bagubits <info@bagubits.it>
2023-05-10 12:40:09 +0200

    fix: added self as first param on bfetch function

--- bagubits/models/base_fetch.py
+++ bagubits/models/base_fetch.py
@@ -5,7 +5,7 @@ from odoo import models
 class BaseFetch(models.AbstractModel):
     _inherit = "base"
 
-    def bfetch(url, method, data = None):
+    def bfetch(self,url, method, data = None):
         req = urllib.request.Request(url=url, data=json.dumps(data).encode('UTF-8') if data else None, method=method ,headers={
             "Content-Type":"application/json",
         })
