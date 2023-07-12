5d90e3e62e1470069970f42054b708a7ad426d93
bagubits <info@bagubits.it>
2023-07-12 12:39:06 +0200

    removed Product custom field and added bagubits module

---
+++ bagubits/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Bagubits',
+    'version': '16.0.0.0.3',
+    'category': 'Uncategorized',
+    'summary': 'Bagubits base extra tools',
+    'sequence': '0',
+    'author': 'Bagubits SRLS',
+    'maintainer': 'Bagubits SRLS',
+    'website': 'https://bagubits.it',
+    'license': 'LGPL-3',
+    'depends': [
+        'base'
+    ],
+    'demo': [],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
