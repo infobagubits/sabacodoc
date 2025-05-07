4ab7a5ef9515948e42a62db3edb712bfde6598f6
bagubits <info@bagubits.it>
2025-05-07 09:53:37 +0200

    chore: some changes on purchase_order_line

--- sabaco_purchase_customization/models/purchase_order_line.py
+++ sabaco_purchase_customization/models/purchase_order_line.py
@@ -3,13 +3,6 @@ from odoo import models, fields
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    # Campo para UdM acquisto (Unidade de Medida de Compra)
-    purchase_uom_id = fields.Many2one(
-        comodel_name='uom.uom',
-        string='UdM acquisto',
-        help='Unidade de medida utilizada para a compra deste produto.'
-    )
-
     # Campo texto livre para UdM acquisto libera (editável)
     purchase_uom_free = fields.Char(
         string='UdM acquisto libera',
