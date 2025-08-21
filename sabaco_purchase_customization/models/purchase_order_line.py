8a678fa5fa624082604be0b9fe2baa8d8e44f9bc
bagubits <info@bagubits.it>
2025-08-21 13:14:14 +0200

    chore: added purchase_order_line

--- sabaco_purchase_customization/models/purchase_order_line.py
+++ sabaco_purchase_customization/models/purchase_order_line.py
@@ -3,8 +3,8 @@ from odoo import models, fields
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    # Campo texto livre para UdM acquisto libera (editável)
+    # Campo testo libero per UdM acquisto libera (modificabile)
     purchase_uom_free = fields.Char(
         string='UdM acquisto libera',
-        help='Campo livre para informar uma unidade de medida de compra personalizada.'
+        help='Campo libero per inserire un\'unità di misura di acquisto personalizzata.'
     )
