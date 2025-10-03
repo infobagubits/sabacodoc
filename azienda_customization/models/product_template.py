e5d009872ed4bf7175fee8a5ede00f69757027e4
bellomatheus <matheus.bellocorrea@bagubits.it>
2025-10-03 17:34:01 +0200

    fix: Usare metodo selection dinamico per campo type

--- azienda_customization/models/product_template.py
+++ azienda_customization/models/product_template.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    @api.model
+    def _selection_type(self):
+        return [
+            ('consu', 'Prodotto'),
+            ('service', 'Servizio'),
+            ('combo', 'Combo'),
+        ]
+
     # Sovrascrive il campo type per cambiare "Consumabile" in "Prodotto"
     type = fields.Selection(
-        selection=[
-            ('consu', "Prodotto"),
-            ('service', "Service"),
-            ('combo', "Combo"),
-        ],
-        string="Product Type",
+        selection='_selection_type',
+        string="Tipologia prodotto",
         required=True,
         default='consu',
     )
