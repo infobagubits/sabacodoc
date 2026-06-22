a062fe96f8c248c1186122c429bb8b16f294f8d0
bagubits <info@bagubits.it>
2026-06-22 10:54:27 +0200

    chore: added l10n_it_iva_differita module

---
+++ l10n_it_iva_differita/models/res_config_settings.py
@@ -0,0 +1,25 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    # Conto IVA differita (da configurare)
+    account_iva_differita_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Conto IVA differita',
+        related='company_id.account_iva_differita_id',
+        readonly=False,
+        domain="[('company_id', '=', company_id)]",
+        help="Conto patrimoniale utilizzato come IVA differita (es. 'IVA sospesa').",
+    )
+
+    # Giornale "Operazioni varie" per la registrazione automatica
+    journal_iva_differita_id = fields.Many2one(
+        comodel_name='account.journal',
+        string='Giornale IVA differita',
+        related='company_id.journal_iva_differita_id',
+        readonly=False,
+        domain="[('company_id', '=', company_id), ('type', '=', 'general')]",
+        help="Giornale usato per le registrazioni automatiche di storno IVA differita.",
+    )
